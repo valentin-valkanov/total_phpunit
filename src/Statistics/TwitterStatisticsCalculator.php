40a7f35206050785cd9fe5f8ea6fc7224d419dd9
Valentin Valkanov <valentinvalkanof@gmail.com>
2024-07-04 10:28:32 +0300

    Refactoring for Easier Testing

---
+++ src/Statistics/TwitterStatisticsCalculator.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace App\Statistics;
+
+use App\Entity\TwitterAccount;
+use App\Utility\DateHelper;
+
+class TwitterStatisticsCalculator
+{
+    public function newFollowersPerWeek(
+        ?TwitterAccount $lastRecord,
+        int $currentFollowerCount,
+        \DateTimeInterface $checkDate
+    ): int
+    {
+        if (!$lastRecord) {
+            return 0;
+        }
+
+        $followerCountDifference = $currentFollowerCount - $lastRecord->getFollowerCount();
+
+        $fullWeeks = DateHelper::weeksBetweenDates($checkDate, $lastRecord->getCreatedAt());
+
+        $newFollowersPerWeek = $followerCountDifference / max($fullWeeks, 1);
+
+        return (int) $newFollowersPerWeek;
+    }
+}
