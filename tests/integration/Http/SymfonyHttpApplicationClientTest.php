e0194ab792f636f031f3baa4751770252666f1c8
Valentin Valkanov <valentinvalkanof@gmail.com>
2024-07-22 22:44:55 +0300

    Testing An Authentication Exception

--- tests/integration/Http/SymfonyHttpApplicationClientTest.php
+++ tests/integration/Http/SymfonyHttpApplicationClientTest.php
@@ -3,7 +3,9 @@
 namespace App\Tests\integration\Http;
 
 
+use App\Http\ApplicationClientException;
 use App\Http\SymfonyHttpApplicationClient;
+use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpClient\HttpClient;
 
@@ -15,8 +17,7 @@ class SymfonyHttpApplicationClientTest extends TestCase
     private const PHPUNIT_ID = 19057969;
     private const BASE_URL = 'https://api.twitter.com/2/';
 
-    /** @test */
-    public function get_retrieves_the_correct_data_from_the_twitter_api(): void
+    public function testGetRetrievesTheCorrectDataFromTheTwitterApi(): void
     {
         // Setup
         $httpClient = HttpClient::create([
@@ -38,4 +39,25 @@ class SymfonyHttpApplicationClientTest extends TestCase
         $this->assertArrayHasKey('public_metrics', $userData);
         $this->assertArrayHasKey('followers_count', $userData['public_metrics']);
     }
+
+    /** @test */
+    public function the_correct_exception_is_thrown_when_a_fake_bearer_token_is_sent(): void
+    {
+        // Setup
+        $httpClient = \Symfony\Component\HttpClient\HttpClient::create([
+            'headers' => ['Authorization' => 'fake_bearer_token']
+        ]);
+
+        $symfonyHttpApplicationClient = new SymfonyHttpApplicationClient($httpClient);
+
+        $url = self::BASE_URL . 'users/' . self::PHPUNIT_ID . '?user.fields=public_metrics';
+
+//        $this->expectExceptionCode(401);
+        $this->expectException(ApplicationClientException::class);
+        $this->expectExceptionMessage('Unauthorized');
+
+        // Do something
+        $response = $symfonyHttpApplicationClient->get($url);
+    }
+
 }
