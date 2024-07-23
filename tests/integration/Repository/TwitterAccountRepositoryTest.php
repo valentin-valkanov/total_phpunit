c27b783013fb98cf06afa0ee2254829d07b0934d
Valentin Valkanov <valentinvalkanof@gmail.com>
2024-07-23 22:57:33 +0300

    Testing Ordered Database Records

---
+++ tests/integration/Repository/TwitterAccountRepositoryTest.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types=1);
+
+namespace App\Tests\integration\Repository;
+
+use App\Entity\TwitterAccount;
+use App\Repository\TwitterAccountRepository;
+use App\Tests\DatabaseDependantTestCase;
+
+class TwitterAccountRepositoryTest extends DatabaseDependantTestCase
+{
+    private TwitterAccountRepository $repository;
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->repository = $this->entityManager->getRepository(TwitterAccount::class);
+    }
+
+    public function testLastRecordReturnsTheCorrectTwitterAccountEntity()
+    {
+        //Arrange
+        $accountId = 99999;
+        $previousAccount = new TwitterAccount();
+        $previousAccount->setTwitterAccountId($accountId);
+        $previousAccount->setUsername('phpunit');
+        $previousAccount->setFollowerCount(1000);
+        $previousAccount->setCreatedAt(date_create_immutable('2021-01-01'));
+        $this->entityManager->persist($previousAccount);
+
+        $currentAccount = new TwitterAccount();
+        $currentAccount->setTwitterAccountId($accountId);
+        $currentAccount->setUsername('phpunit');
+        $currentAccount->setFollowerCount(1000);
+        $currentAccount->setCreatedAt(date_create_immutable('2022-01-01'));
+        $this->entityManager->persist($currentAccount);
+        $this->entityManager->flush();
+
+        //Act
+        $lastRecord = $this->repository->lastRecord($accountId);
+
+        //Assert
+        $this->assertInstanceOf(TwitterAccount::class, $lastRecord);
+        $this->assertSame($lastRecord->getId(), $currentAccount->getId());
+        $this->assertSame(2, $lastRecord->getId());
+    }
+
+
+
+
+
+
+
+
+}
